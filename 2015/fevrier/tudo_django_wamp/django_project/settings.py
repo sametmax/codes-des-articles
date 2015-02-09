8182f4cc8b455b8473029b4125f387bf90a02859
sametmax <lesametlemax@gmail.com>
2015-02-09 09:07:37 +0100

    Serving the static file with crossbar for the last demo

--- 2015/fevrier/tudo_django_wamp/django_project/settings.py
+++ 2015/fevrier/tudo_django_wamp/django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
